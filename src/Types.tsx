00436c1bda76f21bb489c67952d14db77e27ceeb
Raúl Alexander Mendoza Muñoz <wazzu_3000@hotmail.com>
2020-06-22 15:32:17 -0500

    Increment BootstrapTextEdit's type

--- src/Types.tsx
+++ src/Types.tsx
@@ -1,5 +1,5 @@
 export type Dictionary<T> = { [key: string]: T };
-export type InputTypes = 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url';
+export type InputTypes = 'date' | 'datetime-local' | 'email' | 'number' | 'month' | 'password'| 'search' | 'tel' | 'text' | 'url' | 'week';
 export type Autocomplete = 'on' | 'off';
 export type EncTypes = 'application/x-www-form-urlencoded' | 'multipart/form-data' | 'text/plain';
 export type Methods = 'get' | 'post';
